982c25ce1679d4fe91581dabf9ecf7f0a9cc3f13
khushi-217 <Khushirawatsnd2004@gmail.com>
2026-07-16 15:00:42 +0530

    Added sensor data CSV generation

--- python/sensor_simulator.py
+++ python/sensor_simulator.py
@@ -3,7 +3,7 @@ import time
 import csv
 from datetime import datetime
 container_id = "ATM001"
-container_id = "ATM001"
+
 commodity = "Avocado"
 temperature = round(random.uniform(2.0, 8.0), 2)
 
@@ -12,6 +12,29 @@ humidity = round(random.uniform(40.0, 70.0), 2)
 timestamp = datetime.now()
 vibration = random.randint(1, 15)
 battery = random.randint(80, 100)
+# Save sensor data into CSV
+with open("sensor_data.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+
+    writer.writerow([
+        "Container ID",
+        "Commodity",
+        "Temperature",
+        "Humidity",
+        "Timestamp",
+        "Vibration",
+        "Battery"
+    ])
+
+    writer.writerow([
+        container_id,
+        commodity,
+        temperature,
+        humidity,
+        timestamp,
+        vibration,
+        battery
+    ])
 
 print("------ ATMOSYNC SENSOR DATA ------")
 
@@ -39,7 +62,6 @@ with open("dataset/avocado.csv", "r", encoding="utf-8") as file:
     for row in reader:
         print(row)
         count += 1
-
         if count == 5:
             break
 
