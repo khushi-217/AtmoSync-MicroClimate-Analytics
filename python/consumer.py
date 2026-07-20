a74c11a44b0144b98b7665f121239739707e163b
khushi-217 <Khushirawatsnd2004@gmail.com>
2026-07-20 16:37:04 +0530

    Added consumer module for IoT simulator

---
+++ python/consumer.py
@@ -0,0 +1,38 @@
+import csv
+import time
+
+print("=" * 40)
+print("      ATMOSYNC CONSUMER")
+print("=" * 40)
+
+with open("sensor_data.csv", "r") as file:
+
+    reader = csv.DictReader(file)
+
+    count = 1
+
+    for row in reader:
+
+        print(f"\nReceived Record {count}")
+        print("-" * 30)
+
+        print("Container_ID :", row["Container ID"])
+        print("Commodity    :", row["Commodity"])
+        print("Temperature  :", row["Temperature"])
+        print("Humidity     :", row["Humidity"])
+        print("Vibration    :", row["Vibration"])
+        print("Battery      :", row["Battery"])
+        print("Timestamp    :", row["Timestamp"])
+
+        # Alerts
+        if float(row["Temperature"]) > 6:
+            print("⚠ ALERT: High Temperature")
+
+        if int(row["Battery"]) < 85:
+            print("⚠ ALERT: Low Battery")
+
+        count += 1
+
+        time.sleep(0.2)
+
+print("\nConsumer finished receiving all records.")
